52801fcbda7964612531c79282856c87f8c3962c
stephan <stephanruler@gmail.com>
2022-10-05 20:47:35 +0200

    fix: more tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,47 +1,116 @@
 import { CodeBench } from "../src/index";
 
 describe('CodeBench', () => {
-	test('constructor', async () => {
-		const cb = new CodeBench()
-		expect(cb).toBeTruthy()
+	test('maxItrCount', async () => {
+		const cb = new CodeBench({
+			maxItrCount: 1,
+		})
+		let itrCount = 0
+		cb.task("test a", () => {
+			itrCount += 1
+		})
+		await cb.run()
+		expect(itrCount).toBe(2) // 1 x warmup
+	})
+	test('maxItrCount', async () => {
+		const cb = new CodeBench({
+			maxItrCount: 10,
+		})
+		let itrCount = 0
+		cb.task("test a", () => {
+			itrCount += 1
+		})
+		await cb.run()
+		expect(itrCount).toBe(11) // 1 x warmup
+	})
+
+	test('shutdown', async () => {
+		let executionCount = 0
+		const cb = new CodeBench({
+			maxItrCount: 1,
+			startup: async () => {
+				executionCount = executionCount + 1
+			}
+		})
+		let testFailed = false
+		cb.task("test a", () => {
+			if(executionCount !== 1){
+				testFailed = true
+			}
+		})
+		cb.task("test a", () => {
+			if(executionCount !== 1){
+				testFailed = true
+			}
+		})
+		expect(executionCount).toBe(0)
+		await cb.run()
+		expect(testFailed).toBe(false)
+		expect(executionCount).toBe(1)
 	})
 	test('cleanup', async () => {
-		let hasCleanupBeenExcuted = false
+		let executionCount = 0
 		const cb = new CodeBench({
+			maxItrCount: 1,
 			cleanup: async () => {
-				hasCleanupBeenExcuted = true
+				executionCount = executionCount + 1
 			}
 		})
 		let testFailed = false
 		cb.task("test a", () => {
-			if(hasCleanupBeenExcuted){
+			if(executionCount !== 0){
 				testFailed = true
 			}
 		})
 		await cb.run()
 		expect(testFailed).toBe(false)
-		expect(hasCleanupBeenExcuted).toBe(true)
+		expect(executionCount).toBe(1)
 	})
 	test('cleanup between', async () => {
-		let hasCleanupBeenExcuted = 0
+		let executionCount = 0
 		const cb = new CodeBench({
+			maxItrCount: 1,
 			cleanup: async () => {
-				hasCleanupBeenExcuted = hasCleanupBeenExcuted + 1
+				executionCount = executionCount + 1
 			}
 		})
 		let testFailed = false
 		cb.task("test a", () => {
-			if(hasCleanupBeenExcuted !== 0){
+			if(executionCount !== 0){
 				testFailed = true
 			}
 		})
 		cb.task("test a", () => {
-			if(hasCleanupBeenExcuted !== 1){
+			if(executionCount !== 1){
 				testFailed = true
 			}
 		})
 		await cb.run()
 		expect(testFailed).toBe(false)
-		expect(hasCleanupBeenExcuted).toBe(2)
+		expect(executionCount).toBe(2)
+	})
+
+	test('shutdown', async () => {
+		let executionCount = 0
+		const cb = new CodeBench({
+			maxItrCount: 1,
+			shutdown: async () => {
+				executionCount = executionCount + 1
+			}
+		})
+		let testFailed = false
+		cb.task("test a", () => {
+			if(executionCount !== 0){
+				testFailed = true
+			}
+		})
+		cb.task("test a", () => {
+			if(executionCount !== 0){
+				testFailed = true
+			}
+		})
+		await cb.run()
+		expect(testFailed).toBe(false)
+		expect(executionCount).toBe(1)
 	})
 })
